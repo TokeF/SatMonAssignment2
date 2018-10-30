3ea1acefffcd1b8da5741e01fdcad0e8350e4e2b
Toke Frederiksen <tfr027@uit.no>
2018-10-30 13:21:31 +0100

    final version

--- assignment2_1.m
+++ assignment2_1.m
@@ -5,22 +5,26 @@ M = load('flevoland.mat');
 % Use HH as red, XX as green and VV as blue
 R = M.C(1,1,:,:); G = M.C(2,2,:,:); B = M.C(3,3,:,:);
 R = squeeze(R); G = squeeze(G); B = squeeze(B);
+%convert to dB
 R = 10*log10(4*pi*R);
 G = 10*log10(4*pi*G);
 B = 10*log10(4*pi*B);
-% RGB = cat(3,R,G,B);
+%stretch values 
 RGB(:,:,1) = mat2gray(R,[min(min(R)) max(max(R))]);
 RGB(:,:,2) = mat2gray(G,[min(min(G)) max(max(G))]);
 RGB(:,:,3) = mat2gray(B,[min(min(B)) max(max(B))]);
+%plot each channel to check they are correct
 figure(1);
 subplot(1,3,1); imshow(R,[]); title('Red')
 subplot(1,3,2); imshow(G,[]); title('Green')
 subplot(1,3,3); imshow(B,[]); title('Blue')
 
+%rgb image
 figure(2);
 imagesc(RGB)
 title('RGB image')
 
+%gtruth
 figure(3)
 imagesc(M.gtruth)
 colormap(M.cmap)
@@ -30,10 +34,12 @@ title('Ground truth')
 [class, tstClass, trnClass] = splitData(M, 0.6, 2);
 
 %% 1.c histogram
+%define the gamma distribution
 alpha = 27;
 p = @(I, mu) (alpha / mu)^alpha .* I.^(alpha - 1) / factorial(alpha - 1)...
     .* exp(-alpha / mu .* I);
 
+%plot histogram for HH, HV and VV cahnnel for the three classes
 for c = 1 : 3
     cn = [3 5 15];
     s = size(trnClass{cn(c)},3); %number of samples in a class
@@ -41,7 +47,6 @@ for c = 1 : 3
     Ihv = zeros(s,1);
     Ivv = zeros(s,1);
     for i = 1 : s
-        %idx = i*3+1; %to go trough 3 indexes in b
         Ihh(i) = abs(trnClass{cn(c)}(1,1,i));
         Ihv(i) = abs(trnClass{cn(c)}(2,2,i));
         Ivv(i) = abs(trnClass{cn(c)}(3,3,i));
@@ -49,24 +54,24 @@ for c = 1 : 3
     
     figure
     subplot(1,3,1);
-    histogram(Ihh, 'Normalization', 'pdf');
+    histogram(Ihh, 'Normalization', 'pdf', 'linewidth', 0.01, 'edgealpha', 0.4);
     hold on
     Imodel = linspace(0,max(Ihh),length(Ihh));
-    plot(Imodel,p(Imodel,mean(Ihh)))
+    plot(Imodel,p(Imodel,mean(Ihh)), 'linewidth', 1.3)
     title('Ihh')
     
     subplot(1,3,2);
-    histogram(Ihv, 'Normalization', 'pdf');
+    histogram(Ihv, 'Normalization', 'pdf', 'linewidth', 0.01, 'edgealpha', 0.4);
     hold on
     Imodel = linspace(0,max(Ihv),length(Ihv));
-    plot(Imodel,p(Imodel,mean(Ihv)))
+    plot(Imodel,p(Imodel,mean(Ihv)), 'linewidth', 1.3)
     title('Ihv')
     
     subplot(1,3,3);
-    histogram(Ivv, 'Normalization', 'pdf');
+    histogram(Ivv, 'Normalization', 'pdf', 'linewidth', 0.01, 'edgealpha', 0.4);
     hold on
     Imodel = linspace(0,max(Ivv),length(Ivv));
-    plot(Imodel,p(Imodel,mean(Ivv)))
+    plot(Imodel,p(Imodel,mean(Ivv)), 'linewidth', 1.3)
     title('Ivv')
 end
 
@@ -102,14 +107,14 @@ edges = 0.5:1:15.5;
 histogram(label,edges,'normalization', 'probability')
 
 %% 1.e
-%user accuracy:
+%Accuracy:
 totSamp = sum(sum(conf,2));
-userAcc
-prodAcc = diag(conf)' ./ sum(conf)
-totAcc = sum(diag(conf)) / totSamp
+userAcc;
+prodAcc = diag(conf)' ./ sum(conf);
+totAcc = sum(diag(conf)) / totSamp;
 pe = sum(conf)/ totSamp * (sum(conf,2) / totSamp);
 p0 = totAcc;
-kappa = (p0 - pe) / (1 - pe)
+kappa = (p0 - pe) / (1 - pe);
 
 
 
--- assignment2_2.m
+++ assignment2_2.m
@@ -1,73 +1,10 @@
 %% Load data
 M = load('sanfransisco.mat');
 
-%% part 2a
-R = abs(M.sHH); G = abs(M.sHV); B = abs(M.sVV);
-R = 10*log10(4*pi*R);
-G = 10*log10(4*pi*G);
-B = 10*log10(4*pi*B);
-
-% RGB = cat(3,R,G,B);
-RGB(:,:,1) = mat2gray(R,[min(min(R(isfinite(R)))) max(max(R))]);
-RGB(:,:,2) = mat2gray(G,[min(min(G(isfinite(G)))) max(max(G))]);
-RGB(:,:,3) = mat2gray(B,[min(min(B(isfinite(B)))) max(max(B))]);
-figure(1);
-subplot(1,3,1); imshow(R,[]); title('Red')
-subplot(1,3,2); imshow(G,[]); title('Green')
-subplot(1,3,3); imshow(B,[]); title('Blue')
-
-figure(2);
-imagesc(RGB)
-title('RGB image')
-
-%% 2b
-%PCA for each pixel? pixel is 4D reduce it?
-BW = roipoly(RGB);
-
-%find the index of the pixels of the ROI
-idx = find(BW == 1);
-% "one" row of pixels, ie. 3x3xr*c
-linsVV = reshape(M.sVV,2800*2800,1);
-linsHH = reshape(M.sHH,2800*2800,1);
-linsHV = reshape(M.sHV,2800*2800,1);
-linsVH = reshape(M.sVH,2800*2800,1);
-pix = zeros(length(idx),4);
-pix(:,1) = linsVV(idx);
-pix(:,2) = linsHH(idx);
-pix(:,3) = linsHV(idx);
-pix(:,4) = linsVH(idx);
-sX = 1/sqrt(2)*(linsHV + linsVH);
-target = [linsHH sX linsVV];
-%perform pca on the taget vector of the ROI, each pixels is a measurement with 4
-%variables
-coeff = pca(target);
-c1 = coeff(:,1);
-
-% compute the image as a taget vector in 2800x2800x3
-targetImg(:,:,1) = M.sHH;
-targetImg(:,:,2) =  1/sqrt(2)*(M.sHV + M.sVH);
-targetImg(:,:,3) = M.sVV;
-
-innerMat = zeros(2800,2800);
-for i = 1 : 2800
-    i
-    for j = 1 : 2800
-        kl = squeeze(targetImg(i,j,:));
-        innerMat(i,j) = acos( norm(kl' * c1) / norm(kl));
-    end
-end
-%%
-%apply sliding window of size 5x5
-% h = fspecial('average', 5);
-% targetFil = imfilter(innerMat,h);
-targetFil = slidingWindowAvgSAR(innerMat, 5);
-imagesc(mat2gray(cos(targetFil).^-1,[0 pi/2]));
-
-
 %% the covariance matrix 2a
-
+% Calcuate covariance matrix C using function from the previous assignment
 sX = 1/sqrt(2)*(M.sHV + M.sVH);
-%C = calculateC(M.sHH, sX, M.sVV, 5);
+C = calculateC(M.sHH, sX, M.sVV, 5);
 % calcuate sigma by retriving th necessary value from the diagonal of
 % the array contained in the entries of C
 row = size(C,1); col = size(C,2);
@@ -83,6 +20,7 @@ for i = 1 : row
 end
 R = sighh; G = sigx; B = sigvv;
 RGB = zeros(row, col, 3);
+% stretch and plot covariance matrix
 R(~isfinite(R))=-1;
 G(~isfinite(G))=-1;
 B(~isfinite(B))=-1;
@@ -93,6 +31,8 @@ figure
 imshow(RGB)
 
 %% find the coherency matrix 2a
+% compute coherence matrix from the lienar relation between T and C, using
+% the matrix N
 N = 1/sqrt(2) * [1 0 1; 1 0 -1; 0 sqrt(2) 0];
 sizC = size(C,2);
 T = cell(sizC,sizC);
@@ -102,6 +42,7 @@ for i = 1 : sizC
     end
 end
 
+% plot T using the diagonal
 row = size(T,1); col = size(T,2);
 Thh = zeros(row,col);
 Tx = zeros(row,col);
@@ -113,7 +54,7 @@ for i = 1 : row
         Tvv(i,j) = 10*log10(4*pi*abs(T{i,j}(3,3)));
     end
 end
-R = Thh; G = Tx; B = Tvv;
+B = Thh; R = Tx; G = Tvv;
 RGB = zeros(row, col, 3);
 R(~isfinite(R))=-1;
 G(~isfinite(G))=-1;
@@ -124,5 +65,90 @@ RGB(:,:,1) = mat2gray(R,[min(min(R)) max(max(R))]);
 figure
 imshow(RGB)
 
+%% 2b
+%find the ROI
+BW = roipoly(RGB);
+
+%find the index of the pixels of the ROI
+idx = find(BW == 1);
+% "one" row of pixels, ie. 3x3xr*c
+linsVV = reshape(M.sVV,2800*2800,1);
+linsHH = reshape(M.sHH,2800*2800,1);
+linsHV = reshape(M.sHV,2800*2800,1);
+linsVH = reshape(M.sVH,2800*2800,1);
+%store the values of ROI in pix
+pix = zeros(length(idx),4);
+pix(:,1) = linsVV(idx);
+pix(:,2) = linsHH(idx);
+pix(:,3) = linsHV(idx);
+pix(:,4) = linsVH(idx);
+%compute sX and target vector
+sX = 1/sqrt(2)*(linsHV + linsVH);
+target = [linsHH sX linsVV];
+%perform pca on the taget vector of the ROI, each pixels is a measurement
+%with 3 variables
+coeff = pca(target);
+c1 = coeff(:,1);
+
+% compute the image as a taget vector in 2800x2800x3
+targetImg(:,:,1) = M.sHH;
+targetImg(:,:,2) =  1/sqrt(2)*(M.sHV + M.sVH);
+targetImg(:,:,3) = M.sVV;
+
+%Find the angle of each pixel wr. to the first PCA component
+innerMat = zeros(2800,2800);
+for i = 1 : 2800
+    i
+    for j = 1 : 2800
+        kl = squeeze(targetImg(i,j,:));
+        innerMat(i,j) = acos( norm(kl' * c1) / norm(kl));
+    end
+end
 %%
-[v, l] = eig(R);
+%apply sliding window of size 5x5 and downsampling
+targetFil = slidingWindowAvgSAR(innerMat, 5);
+imagesc(mat2gray(cos(targetFil).^-1,[0 pi/2]));
+
+%% H and alpha decomposition 2c
+% Compute H for each pixel according to the definition
+log3 = @(x) log(x) / log(3);
+H = zeros(row,col);
+alpha = zeros(row,col);
+for i = 1: row
+    for j = 1 : col
+        p = zeros(1,3);
+        [v, lam] = eig(T{i,j});
+        for k = 1 : 3
+            p(k) = lam(1,1) / sum(diag(lam));
+        end
+        H(i,j) = - (p(1) * log3(p(1)) + p(2) * log3(p(2)) + p(3) * log3(p(3)));
+        alpha(i,j) = acos(abs(v(1,1)));
+    end
+end
+alpha = rad2deg(alpha);
+%%
+Hvec = reshape(abs(H), 560*560, 1);
+Avec = reshape(alpha, 560*560, 1);
+scatter(Hvec(1:50:313600), Avec(1:50:313600))
+xlabel('H values'); ylabel('alpha values')
+
+%% part 2d
+close all;
+% I don't know how to compute alhpa, so use the angle from part 2b instead
+theta = mat2gray(cos(targetFil).^-1,[0 pi/2]);
+thetaM = reshape(theta, 560*560,1);
+
+figure
+% perform k-means using H and theta as features for each pixel
+klbls = kmeans( [Hvec, Avec, thetaM] ,5);
+imagesc(reshape(klbls, 560, 560))
+
+figure
+% perform k-means using H and theta as features for each pixel
+klbls = kmeans( [Hvec, thetaM] ,3);
+imagesc(reshape(klbls, 560, 560))
+
+figure
+% perform k-means using H and alpha as features for each pixel
+klbls = kmeans( [Hvec, Avec] ,3);
+imagesc(reshape(klbls, 560, 560))
